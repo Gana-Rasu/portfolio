022126f44c0a2a2f99496119edd08dfa435ee38b
gana <gananathanthirunavukkarasu@gmail.com>
2023-02-03 11:56:12 +0530

    intro

--- src/components/introduction/Intro.js
+++ src/components/introduction/Intro.js
@@ -1,25 +1,33 @@
 import "./Intro.css";
+import Me from './images/address.png';
 
 const Intro = () => {
   return (
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello, my name is</h2>
+          {/* <h2 className="i-intro">Hello, my name is</h2> */}
           <h1 className="i-name">Gananathan</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
-              <div className="i-title-item">WEB DEVELOPER</div>
-              <div className="i-title-item">HTML</div>
-              <div className="i-title-item">CSS</div>
-              <div className="i-title-item">JAVASCRIPT</div>
-              <div className="i-title-item">REACT</div>
-              <div className="i-title-item">MONGO DB</div>
+              <div className="i-title-item">WEB - DEVELOPER</div>
+              <div className="i-title-item"> LANGUAGE : HTML - CSS - JAVASCRIPT</div>
+              <div className="i-title-item"> LIBRARY : REACT JS</div>
+              <div className="i-title-item"> DATABASE : MONGO DB</div>
+              <div className="i-title-item"> SERVER : NODE & EXPRESS JS</div>
+             
             </div>
+          </div>
+          <div className="i-desc">
+            I have learnt web development through an online course and I have
+            completed many complicated assignments and projects, so I am
+            confident that I can add value to the job.
           </div>
         </div>
       </div>
-      <div className="i-right">right</div>
+      <div className="i-right">
+        <img src={Me} alt="" className=""></img>
+      </div>
     </div>
   );
 };
